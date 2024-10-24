7698eadc3619167e61e9733553e17724267c739f
theshpio <student-shpio@proton.me>
2024-10-25 01:19:05 +0200

    feat(ci): Add coveralls, badge, cache

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Blockchain-based package manager, in Rust.
 # navigate to the repository Settings-> Secrets and variables -> Actions -> Variables (Repository variables)
 # and update value for SKIP_CRATE variable.
 ```
+[![Coverage Status](https://coveralls.io/repos/github/theshpio/bpm/badge.svg?branch=main)](https://coveralls.io/github/theshpio/bpm?branch=main)
+
+[![Coverage Status](https://coveralls.io/repos/github/theshpio/bpm/badge.svg?branch=feat/coveralls-badge-cache)](https://coveralls.io/github/theshpio/bpm?branch=feat/coveralls-badge-cache)
--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# exist the script if any command fails
+set -e
+
 # Check if SKIP_CRATE is set
 if [ -z "$SKIP_CRATE" ]; then
   echo "No crate to skip. Running tarpaulin for all crates."
@@ -23,5 +26,13 @@ for crate in $(find packages -name Cargo.toml -exec dirname {} \;); do
   
   cargo tarpaulin --manifest-path $crate/Cargo.toml --out lcov \
     --output-dir $crate/coverage --exclude-files "/target/**/*" "/tarpaulin_temp/**/*" \
-    --target-dir $crate/tarpaulin_temp --skip-clean || echo "Tarpaulin failed for $CRATE_NAME"
+    --target-dir $crate/tarpaulin_temp --skip-clean 
+    
+  # Check if the coverage report exists and upload it
+  if [ -f "$crate/coverage/lcov.info" ]; then
+    echo "Uploading coverage for $CRATE_NAME"
+    coveralls report "$crate/coverage/lcov.info" --repo-token $COVERALLS_REPO_TOKEN
+  else
+    echo "No lcov.info file found for $CRATE_NAME"
+  fi
 done
